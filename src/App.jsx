371c9ca4b61e5a4329577b68916c08dc148945bb
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-06 15:58:31 +0630

    initialize of the search movie page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchMovies from "./components/SearchMovies";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
+      </div>
+    );
+  }
+}
+export default App;
